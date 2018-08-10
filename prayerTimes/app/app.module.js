a1c9a02f85fe3ef5ecf053ff948bc3ca1181a938
rimanirfan <irfanriman@gmail.com>
2018-08-10 16:04:59 +0700

    diplay as table and add styling to index.html

--- prayerTimes/app/app.module.js
+++ prayerTimes/app/app.module.js
@@ -3,32 +3,31 @@ let d         = new Date()
 app.controller("PrayerTimesController", function($http, $scope){
     $scope.latitude       = ''
     $scope.longitude      = ''
-    $scope.dateList       = []
-    $scope.fajrs          = []
-    $scope.dhuhrs         = []
-    $scope.asrs           = []
-    $scope.maghribs       = []
-    $scope.ishas          = []
+    $scope.timings        = []
     if(navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position){
             $scope.$apply(function(){
                 $scope.latitude  = position.coords.latitude
                 $scope.longitude = position.coords.longitude
             })
+            console.log(position)
             $http({
                 url     : "http://api.aladhan.com/v1/calendar",
                 method  : "GET",
                 params  : {latitude: $scope.latitude, longitude: $scope.longitude, month:d.getUTCMonth()+1, year:d.getFullYear()}
             }).then(function(response) {
                 response.data.data.forEach((element, index) => {
-                    $scope.fajrs.push(element.timings.Fajr)
-                    $scope.dhuhrs.push(element.timings.Dhuhr)
-                    $scope.asrs.push(element.timings.Asr)
-                    $scope.maghribs.push(element.timings.Maghrib)
-                    $scope.ishas.push(element.timings.Isha)
-                    $scope.dateList.push(response.data.data[index].date.readable)
+                    let data = {
+                        date    : element.date.readable,
+                        fajr    : element.timings.Fajr,
+                        dhuhr   : element.timings.Dhuhr,
+                        asr     : element.timings.Asr,
+                        maghrib : element.timings.Maghrib,
+                        isha    : element.timings.Isha
+                    }
+                    $scope.timings.push(data)
                 });
-                $scope.prayerTime =  response
+                console.log(response.data.data)
             })
         })
     }
