f6d1c1e506fefa58a912c5179378e40fea667a37
rimanirfan <irfanriman@gmail.com>
2018-08-20 10:34:32 +0700

    implement add post,show post, orderby votes, show comment

--- redditClone/app/app.js
+++ redditClone/app/app.js
@@ -1,21 +1,61 @@
-let app = angular.module('redditApp', [])
+let app = angular.module('redditApp', [
+    "ngStorage"
+])
 
 app.component('reddit', {
-    controller: function($scope) {
+    controller: function(
+        $scope,
+        $localStorage
+    ) {
         // const self = this
         $scope.tab   = 1 
         $scope.posts = []
 
+        let current = [
+            {
+                title       : 'Title 1',
+                description : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, pariatur.',
+                author      : 'Me',
+                imgUrl      : 'https://images.pexels.com/photos/1322185/pexels-photo-1322185.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+                createdOn   : Date.now(),
+                votes       : 1,
+                comments    : [
+                    {
+                        text: 'This is awesome' 
+                    },
+                    {
+                        text: 'This is awesome' 
+                    }
+                ]
+            },
+            {
+                title       : 'Title 2',
+                description : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, pariatur.',
+                author      : 'Me',
+                imgUrl      : 'https://images.pexels.com/photos/1322185/pexels-photo-1322185.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+                createdOn   : Date.now(),
+                votes       : 6,
+                comments    : [
+                    {
+                        text: 'This is awesome 2' 
+                    }
+                ]
+            }
+        ]
+
+        $scope.posts = current
+        console.log($scope.posts)
+        
         $scope.setTab = function(selected) {
             $scope.tab = selected
         }
-
+        
         $scope.isSet = function(tab) {
             return $scope.tab === tab
         }
-
-        $scope.submitPost = function() {
-            console.log('ok')
+        
+        $scope.submitPost = function($event) {
+            $event.preventDefault()
             let data = {
                 title       : $scope.title,
                 description : $scope.description,
@@ -25,12 +65,27 @@ app.component('reddit', {
                 votes       : 0,
                 comments    : []
             }
-            console.log(data)
+
             $scope.posts.push(data)
+            $localStorage.data = $scope.posts
+            ClearModel()
             
         }
-        console.log($scope.title)
-        console.log($scope.posts)
+
+        $scope.showPosts = function() {
+            if($localStorage.data) {
+                $scope.posts = $localStorage.data
+            } else {
+                return
+            }
+        }
+
+        function ClearModel(){
+            $scope.title       = null
+            $scope.description = null
+            $scope.author      = null
+            $scope.imgUrl      = null
+        }
     },
     templateUrl: 'reddit.html'
 })
