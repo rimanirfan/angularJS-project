2dd83fe70667ed40cb95a1ca7c7fa3d10ebe7673
rimanirfan <irfanriman@gmail.com>
2018-08-21 10:03:24 +0700

    implement component for file organization

---
+++ todoApp/app/todoapp.component.js
@@ -0,0 +1,63 @@
+angular.
+    module("todoApp").
+    component("todoApp", {
+        templateUrl: "todoapp.template.html",
+        controller : function TodoAppController(
+            $scope,
+            $localStorage,
+            $sessionStorage
+        ){
+            $scope.todoList = []
+            let self        = this
+            self.id         = 0
+            $scope.addTodo = function(){
+                
+                let todo = {
+                    id       : ++self.id,
+                    name     : $scope.todo,
+                    priority : $scope.priority
+                }
+
+                $scope.todoList.push(todo)
+                $localStorage.data = $scope.todoList
+                ClearModel() //this will reset all values to default
+            }
+
+            $scope.deleteTodo = function(todo){
+                let index = $scope.todoList.indexOf(todo)
+
+                $scope.todoList.splice(index,1)
+
+            }
+
+            $scope.BindSelectedData = function(todo) {
+                $scope.id       = todo.id
+                $scope.todo     = todo.name
+                $scope.priority = todo.priority
+            }
+
+            $scope.updateTodo = function(todo){
+                $.grep($scope.todoList, function(e){
+                    if(e.id == $scope.id) {
+                        e.name     = $scope.todo
+                        e.priority = $scope.priority
+                    }
+                    ClearModel() //this will reset all values to default
+                })
+            }
+
+            $scope.showTodo = function() {
+                if($localStorage.data) {
+                    $scope.todoList = $localStorage.data
+                } else {
+                    return
+                }
+            }
+
+            function ClearModel(){
+                $scope.id       = null
+                $scope.todo     = null
+                $scope.priority = null
+            }
+        }
+    })
