c05eaa3e9fc696969bc21ff0c4ebd903832826b2
rimanirfan <irfanriman@gmail.com>
2018-08-21 10:12:20 +0700

    implement component for file organization

---
+++ prayerTimes/app/prayertimes.component.js
@@ -0,0 +1,41 @@
+let d         = new Date()
+angular.
+    module('prayerApp').
+    component('prayerApp', {
+        templateUrl: "prayerapp.template.html",
+        controller : function PrayerTimesController(
+            $http,
+            $scope
+        ) {
+            $scope.latitude       = ''
+            $scope.longitude      = ''
+            $scope.timings        = []
+            if(navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(function(position){
+                    $scope.$apply(function(){
+                        $scope.latitude  = position.coords.latitude
+                        $scope.longitude = position.coords.longitude
+                    })
+                    console.log(position)
+                    $http({
+                        url     : "http://api.aladhan.com/v1/calendar",
+                        method  : "GET",
+                        params  : {latitude: $scope.latitude, longitude: $scope.longitude, month:d.getUTCMonth()+1, year:d.getFullYear()}
+                    }).then(function(response) {
+                        response.data.data.forEach((element, index) => {
+                            let data = {
+                                date    : element.date.readable,
+                                fajr    : element.timings.Fajr,
+                                dhuhr   : element.timings.Dhuhr,
+                                asr     : element.timings.Asr,
+                                maghrib : element.timings.Maghrib,
+                                isha    : element.timings.Isha
+                            }
+                            $scope.timings.push(data)
+                        });
+                        console.log(response.data.data)
+                    })
+                })
+            }
+        }
+    })
